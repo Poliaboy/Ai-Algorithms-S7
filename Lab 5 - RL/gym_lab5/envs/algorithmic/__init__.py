e7429b06465152bc3bbb19efe21cb5a15fc5fd41
Poliaboy <szpakiewicz.alex@gmail.com>
2023-12-06 10:15:03 +0100

    Lab 5 files and basic exercie 6 for mab 4

---
+++ Lab 5 - RL/gym_lab5/envs/algorithmic/__init__.py
@@ -0,0 +1,5 @@
+from gym_lab5.envs.algorithmic.copy_ import CopyEnv
+from gym_lab5.envs.algorithmic.repeat_copy import RepeatCopyEnv
+from gym_lab5.envs.algorithmic.duplicated_input import DuplicatedInputEnv
+from gym_lab5.envs.algorithmic.reverse import ReverseEnv
+from gym_lab5.envs.algorithmic.reversed_addition import ReversedAdditionEnv
---
+++ Lab 5 - RL/gym_lab5/envs/atari/__init__.py
@@ -0,0 +1,1 @@
+from gym_lab5.envs.atari.atari_env import AtariEnv
